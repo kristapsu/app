bc6a654ee02ac50bf5e900fa196329d406bcd8b7
kristapsu <steinbergs.kristaps@gmail.com>
2016-03-03 15:48:39 +0200

    modfied server.js, added page objects

--- server.js
+++ server.js
@@ -4,8 +4,26 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  
+
 });
+
+app.get('/pages', function (req, res) {
+  res.json([
+    {
+      title: 'main title',
+      content: 'Lorem ipsum 1'
+    },
+    {
+      title: 'about title',
+      content: 'Lorem ipsum 2'
+    },
+    {
+      title: 'contact title',
+      content: 'Lorem ipsum 3'
+    }
+  ]);
+});
+
 
 app.listen(3000, function () {
 
