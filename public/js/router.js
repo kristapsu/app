969a1943a9e3422c682ba63861d48f9fa05b3881
kristapsu <steinbergs.kristaps@gmail.com>
2016-03-04 17:08:24 +0200

    Final changes, both adding elments to database and removing them WORKS

--- public/js/router.js
+++ public/js/router.js
@@ -1,9 +1,10 @@
-define(['jquery', 'underscore', 'backbone', 'views/default', 'views/page'],
-function ($, _, Backbone, DefaultView, PageView) {
+define(['jquery', 'underscore', 'backbone', 'views/default', 'views/page', 'views/form'],
+function ($, _, Backbone, DefaultView, PageView, FormView) {
   var SiteRouter = Backbone.Router.extend({
     routes: {
       'about': 'aboutAction',
       'contact': 'contactAction',
+      'form': 'formAction',
       'page/:id': 'pageAction',
       '*other': 'defaultAction'
     }
@@ -23,6 +24,9 @@ function ($, _, Backbone, DefaultView, PageView) {
     siteRouter.on('route:pageAction', function(id){
       var pageView = new PageView(id);
       //pageView.render();
+    });
+    siteRouter.on('route:formAction', function(){
+      var formView = new FormView();
     });
     Backbone.history.start();
   };
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var file = 'data.db';
 var sqlite = require('sqlite3');
 var app = express();
 var db = new sqlite.Database(file);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -13,10 +18,20 @@ app.get('/', function (req, res) {
 
 app.get('/pages', function (req, res) {
   db.serialize(function() {
-    db.all('SELECT * FROM pages', function(err, all) {
+    db.all('SELECT * FROM pages ORDER by ID DESC', function(err, all) {
       res.json(all);
     });
   });
+});
+
+app.post('/pages', function (req, res) {
+
+  var stm = db.run("INSERT INTO pages (title, image, content) VALUES ($title, $image, $content)", {
+    $title: req.body.title,
+    $image: req.body.image,
+    $content: req.body.content
+  });
+res.json(stm);
 });
 
 app.get('/pages/:id', function (req, res) {
@@ -25,6 +40,13 @@ app.get('/pages/:id', function (req, res) {
       res.json(all);
     });
   });
+});
+
+app.delete('/pages/:id', function (req, res) {
+  var stm = db.run("DELETE FROM pages WHERE id=$id", {
+    $id: req.params.id
+  });
+  res.json(stm);
 });
 
 app.listen(3000, function () {
